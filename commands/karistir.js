3563497adfb88266be66fe1d3d66888987c845c4
Skaytouch <60575794+Skaytouch@users.noreply.github.com>
2024-03-15 05:47:30 +0300

    Update and rename shuffle.js to karistir.js

--- commands/karistir.js
+++ commands/karistir.js
@@ -2,18 +2,18 @@ const { EmbedBuilder } = require("discord.js")
 const config = require("../config.js");
 const db = require("../mongoDB");
 module.exports = {
-  name: "shuffle",
-  description: "Shuffle the queue songs",
+  name: "karistir",
+  description: "Sıra şarkılarını karıştırın",
   options: [],
   permissions: "0x0000000000000800",
   run: async (client, interaction) => {
     try {
 
         const queue = client.player.getQueue(interaction.guild.id);
-        if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+        if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ Müzik çalmıyor!!`, ephemeral: true }).catch(e => { })
         try {
           queue.shuffle(interaction)
-        return interaction.reply({ content: `<@${interaction.user.id}>, Shuffled the songs for You!!` }).catch(e => { })
+        return interaction.reply({ content: `<@${interaction.user.id}>, Sıralamayı karıştırdım!!` }).catch(e => { })
         } catch(err) {
         return interaction.reply({ content: `**${err}**` }).catch(e => { })
         }
