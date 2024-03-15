fd5b21a536429a2b7912db32b224efd44548df0d
Skaytouch <60575794+Skaytouch@users.noreply.github.com>
2024-03-15 23:31:14 +0300

    Update playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -7,32 +7,16 @@ module.exports = async (client, queue, song) => {
     if (queue?.textChannel) {
       const embed = new EmbedBuilder()
       .setAuthor({
-        name: 'Currently playing a Track',
-        iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        name: 'Şu anda bir parça çalınıyor',
+  
     })
-    .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
-.setImage(queue.songs[0].thumbnail)
+    .setDescription(`\n ‎ \n▶️ **Detaylar:** **${song?.name}**\n▶️ **Müziğin En Üstün Deneyimini Yaşayın. ** \n▶️ **Eğer bağlantı bozulursa, sorgu verin.**`)
+    .setImage(queue.songs[0].thumbnail)
     .setColor('#FF0000')
-    .setFooter({ text: 'More info - Use /help command [RTX GAMING]' });
+    .setFooter({ text: 'Daha fazla bilgi - /yardim komutunu kullanın ' });
      
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
   }
 }
 
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
