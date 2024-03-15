96c8b614652cc8f3fda8e3ca92d2c200a37be4b0
Skaytouch <60575794+Skaytouch@users.noreply.github.com>
2024-03-15 23:28:01 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,41 +1,40 @@
 const config = require("../config.js");
-const { ActivityType  } = require("discord.js")
+const { ActivityType } = require("discord.js");
+
 module.exports = async (client) => {
+  if (config.mongodbURL || process.env.MONGO) {
+    const { REST } = require("@discordjs/rest");
+    const { Routes } = require("discord-api-types/v10");
+    const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
 
+    (async () => {
+      try {
+        await rest.put(Routes.applicationCommands(client.user.id), {
+          body: await client.commands,
+        });
+        console.log('\x1b[36m%s\x1b[0m', '|    🚀 Komutlar Yüklendi!')
+      } catch (err) {
+        console.log('\x1b[36m%s\x1b[0m', '|    🚀 Komutlar Dağıtıldı!');
+      }
+    })();
 
-if (config.mongodbURL || process.env.MONGO) {
+    console.log('\x1b[32m%s\x1b[0m', `|    🌼 ${client.user.username} olarak giriş yapıldı`);
 
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v10");
-const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
-(async () => {
-try {
-await rest.put(Routes.applicationCommands(client.user.id), {
-body: await client.commands,
-});
-console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded!')
-} catch (err) {
-console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
-}
-})();
+    setInterval(() => client.user.setActivity({
+      name: `/yardim oynuyor`,
+      type: ActivityType.Watching
+    }), 10000);
 
-console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+    client.errorLog = config.errorLog;
+  } else {
+    console.log('\x1b[36m%s\x1b[0m', `|    🍔 MongoDB Hatası!`)
+  }
+  console.log('\x1b[36m%s\x1b[0m', `|    🎯 Etkinlik başarıyla ayarlandı!`);
 
-setInterval(() => client.user.setActivity({ 
-  name: `Update v3.6`, 
-  type: ActivityType.Watching }), 10000);
-client.errorLog = config.errorLog
-} else {
-console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
-}
-console.log('\x1b[36m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
-
-
-if(client.config.voteManager.status === true && client.config.voteManager.api_key){
-const { AutoPoster } = require('topgg-autoposter')
-const ap = AutoPoster(client.config.voteManager.api_key, client)
-ap.on('posted', () => {
-})
-}
-
+  if (client.config.voteManager.status === true && client.config.voteManager.api_key) {
+    const { AutoPoster } = require('topgg-autoposter');
+    const ap = AutoPoster(client.config.voteManager.api_key, client);
+    ap.on('posted', () => {
+    });
+  }
 }
