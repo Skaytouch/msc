2ab18b23695f566af4ace997aee29119436d9b40
Skaytouch <60575794+Skaytouch@users.noreply.github.com>
2024-03-15 23:36:09 +0300

    Update yardim.js

--- commands/yardim.js
+++ commands/yardim.js
@@ -16,10 +16,10 @@ module.exports = {
         .setColor(client.config.embedColor)
         .setTitle('🎸 **Müzik Komutları**')
         .addFields(
-          { name: '🎹 Play', value: 'Belirtilen linkten veya kaynaklardan bir şarkıyı yayınla' },
+          { name: '🎹 Çal', value: 'Belirtilen linkten veya kaynaklardan bir şarkıyı yayınla' },
           { name: '⏹️ Dur', value: 'Botun müziği durdurmasını ve ses kanalından çıkmasını sağlar' },
-          { name: '📊 Kuyruk', value: 'Bu sunucunun şarkı kuyruğunu görüntüleyip yönetin' },
-          { name: '⏭️ Atlama', value: 'Mevcut çalan şarkıyı atla' },
+          { name: '📊 Sira', value: 'Bu sunucunun şarkı kuyruğunu görüntüleyip yönetin' },
+          { name: '⏭️ Atla', value: 'Mevcut çalan şarkıyı atla' },
           { name: '⏸️ Duraklat', value: 'Şu anda çalan şarkıyı duraklat' },
           { name: '▶️ Devam Et', value: 'Duraklatılmış şarkıyı devam ettir' },
           { name: '🔁 Döngü', value: 'Kuyruk ve mevcut şarkı için döngü modunu açıp kapatın' },
@@ -39,28 +39,9 @@ module.exports = {
           { name: '🗑️ Temizle', value: 'Bu sunucunun şarkı kuyruğunu temizleyin' },
           { name: '⏱️ Zaman', value: 'Mevcut şarkı çalma zamanını görüntüle' },
           { name: '🎧 Filtre', value: 'Sesinizi artırmak için filtreler uygulayın' },
-          { name: '🎵 Şimdi Çalan', value: 'Şu anda çalınan şarkı bilgilerini görüntüle' },
+          { name: '🎵 Çalan', value: 'Şu anda çalınan şarkı bilgilerini görüntüle' },
           { name: '🔊 Ses', value: 'Müzik sesini ayarlayın [yüksek sesle dinlemek risklidir]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
-      const button1 = new ButtonBuilder()
-        .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
-        .setStyle(ButtonStyle.Link);
-
-      const button2 = new ButtonBuilder()
-        .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
-        .setStyle(ButtonStyle.Link);
-
-      const button3 = new ButtonBuilder()
-        .setLabel('Kod')
-        .setURL('https://github.com/RTX-GAMINGG/RTX-MUSIC-BOT-v3.6')
-        .setStyle(ButtonStyle.Link);
-
-      const row = new ActionRowBuilder()
-        .addComponents(button1, button2, button3);
-
       interaction.reply({
         embeds: [musicCommandsEmbed, basicCommandsEmbed],
         components: [row]
