2f57cb7f9594778985cf30451335d3b87ee92f6b
Skaytouch <60575794+Skaytouch@users.noreply.github.com>
2024-03-15 05:22:27 +0300

    Update yardim.js

--- commands/yardim.js
+++ commands/yardim.js
@@ -12,39 +12,57 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const müzikKomutlarıEmbed = new EmbedBuilder()
+      const musicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
         .setTitle('🎸 **Müzik Komutları**')
         .addFields(
-          { name: '🎹 Oynat', value: 'Verilen bir bağlantıdan veya kaynaklardan bir şarkıyı yayınla' },
+          { name: '🎹 Play', value: 'Belirtilen linkten veya kaynaklardan bir şarkıyı yayınla' },
           { name: '⏹️ Dur', value: 'Botun müziği durdurmasını ve ses kanalından çıkmasını sağlar' },
-          { name: '📊 Kuyruk', value: 'Bu sunucunun şarkı kuyruğunu görüntüle ve yönet' },
-          { name: '⏭️ Atlama', value: 'Mevcut çalan şarkıyı atlamak için kullan' },
-          { name: '⏸️ Duraklat', value: 'Şu anda çalan şarkıyı duraklatır' },
-          { name: '▶️ Devam', value: 'Duraklatılan şarkıyı devam ettirir' },
-          { name: '🔁 Döngü', value: 'Kuyruk ve mevcut şarkı için döngü modunu açar/kapatır' },
-          { name: '🔄 Otomatik Oynatma', value: 'Otomatik oynatmayı etkinleştirir veya devre dışı bırakır [rastgele şarkıları çalar]' },
-          { name: '⏩ Arama', value: 'Mevcut şarkıda belirli bir zamana git' },
-          { name: '⏮️ Önceki', value: 'Sıradaki şarkıyı çalar' },
-          { name: '🔀 Karıştır', value: 'Kuyruktaki şarkıları karıştırır' },
-          { name: '📃 Çalma Listesi', value: 'çalma listelerini yönetir' }
+          { name: '📊 Kuyruk', value: 'Bu sunucunun şarkı kuyruğunu görüntüleyip yönetin' },
+          { name: '⏭️ Atlama', value: 'Mevcut çalan şarkıyı atla' },
+          { name: '⏸️ Duraklat', value: 'Şu anda çalan şarkıyı duraklat' },
+          { name: '▶️ Devam Et', value: 'Duraklatılmış şarkıyı devam ettir' },
+          { name: '🔁 Döngü', value: 'Kuyruk ve mevcut şarkı için döngü modunu açıp kapatın' },
+          { name: '🔄 Otomatik Çalma', value: 'Otomatik oynatmayı etkinleştir veya devre dışı bırakın [rastgele şarkılar çal]' },
+          { name: '⏩ İlerlet', value: 'Mevcut şarkıda belirli bir zaman dilimine ilerle' },
+          { name: '⏮️ Önceki', value: 'Kuyruktaki bir önceki şarkıyı çal' },
+          { name: '🔀 Karıştır', value: 'Kuyruktaki şarkıları karıştır' },
+          { name: '📃 Çalma Listesi', value: 'çalma listelerini yönetin' }
         )
+        .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
 
-      const temelKomutlarEmbed = new EmbedBuilder()
+      const basicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
         .setTitle('✨ **Temel Komutlar**')
         .addFields(
           { name: '🏓 Ping', value: "Botun gecikmesini kontrol edin" },
-          { name: '🗑️ Temizle', value: 'Bu sunucunun şarkı kuyruğunu temizler' },
-          { name: '⏱️ Zaman', value: 'Mevcut şarkının oynatma süresini gösterir' },
-          { name: '🎧 Filtre', value: 'Ses kalitesini artırmak için filtreleri uygular' },
-          { name: '🎵 Şimdi Çalıyor', value: 'Mevcut çalan şarkı bilgilerini gösterir' },
-          { name: '🔊 Ses', value: 'Müzik sesini ayarlar [yüksek sesle dinlemek risklidir]' },
+          { name: '🗑️ Temizle', value: 'Bu sunucunun şarkı kuyruğunu temizleyin' },
+          { name: '⏱️ Zaman', value: 'Mevcut şarkı çalma zamanını görüntüle' },
+          { name: '🎧 Filtre', value: 'Sesinizi artırmak için filtreler uygulayın' },
+          { name: '🎵 Şimdi Çalan', value: 'Şu anda çalınan şarkı bilgilerini görüntüle' },
+          { name: '🔊 Ses', value: 'Müzik sesini ayarlayın [yüksek sesle dinlemek risklidir]' },
         ) 
-  
+       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+      const button1 = new ButtonBuilder()
+        .setLabel('YouTube')
+        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setStyle(ButtonStyle.Link);
+
+      const button2 = new ButtonBuilder()
+        .setLabel('Discord')
+        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setStyle(ButtonStyle.Link);
+
+      const button3 = new ButtonBuilder()
+        .setLabel('Kod')
+        .setURL('https://github.com/RTX-GAMINGG/RTX-MUSIC-BOT-v3.6')
+        .setStyle(ButtonStyle.Link);
+
+      const row = new ActionRowBuilder()
+        .addComponents(button1, button2, button3);
 
       interaction.reply({
-        embeds: [müzikKomutlarıEmbed, temelKomutlarEmbed],
+        embeds: [musicCommandsEmbed, basicCommandsEmbed],
         components: [row]
       }).catch(e => {});
     } catch (e) {
