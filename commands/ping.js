c52a24b0108a28c75cc9d8aaeab43caf0de53e04
Skaytouch <60575794+Skaytouch@users.noreply.github.com>
2024-03-15 05:28:51 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,57 +1,24 @@
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
 const { EmbedBuilder } = require('discord.js')
 const db = require("../mongoDB");
+
 module.exports = {
   name: "ping",
-  description: "check the bot latency",
+  description: "bot gecikmesini kontrol eder",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
-
-
     try {
-
-      const start = Date.now();
-      interaction.reply("Pinging....").then(msg => {
-        const end = Date.now();
+      const başlangıç = Date.now();
+      interaction.reply("Pingleme....").then(msg => {
+        const son = Date.now();
         const embed = new EmbedBuilder()
           .setColor(`#6190ff`)
-          .setTitle(`Bot Latency`)
-          .setDescription(`**Pong** : ${end - start}ms`)
+          .setTitle(`Bot Gecikmesi`)
+          .setDescription(`**Pong** : ${son - başlangıç}ms`)
         return interaction.editReply({ embeds: [embed] }).catch(e => { });
       }).catch(err => { })
-
     } catch (e) {
-    console.error(e); 
-  }
+      console.error(e); 
+    }
   },
 };
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
